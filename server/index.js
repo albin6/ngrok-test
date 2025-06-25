23a48fe27e877d97333b685b39959064b4f4124d
Albin Aji <albinpariyarathu@gmail.com>
2025-06-25 09:40:01 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import { connectDB } from './config/db'
+import { connectDB } from './config/db.js'
 dotenv.config()
 
 connectDB()
