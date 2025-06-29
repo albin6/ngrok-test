0742e5c9724c3047e53f9d9e9be564c022be941a
Albin Aji <albinpariyarathu@gmail.com>
2025-06-30 01:01:45 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/db.js'
-import { Message } from './models/message.model.js'
-dotenv.config()
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import { Message } from "./models/message.model.js";
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/hello', async (req, res) => {
-    const id = req.query.id
-    console.log(`Received request for ID: ${id}`, typeof id)
+app.get("/api/hello", async (req, res) => {
+  const id = req.query.id;
+  console.log(`Received request for ID: ${id}`, typeof id);
 
-    const messages = await Message.find()
-    console.log(`Messages found: ${messages.length}`, messages)
-    res.json({ messages: messages })
-})
+  const messages = await Message.find();
+  console.log(`Messages found: ${messages.length}`, messages);
+  res.json({ messages: messages });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
