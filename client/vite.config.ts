5487fbee2f40ea6626a71e300e3c91b0ee2db40c
Albin Aji <albinpariyarathu@gmail.com>
2025-07-02 18:29:49 +0530

    modified:   client/vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["132d-103-214-235-16.ngrok-free.app"], // Replace with your actual ngrok URL
+    allowedHosts: ["777a-103-214-235-23.ngrok-free.app"], // Replace with your actual ngrok URL
     proxy: {
       "/api": {
         target: "http://localhost:5000",
